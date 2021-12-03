06d9d4f5434a27b0e463d4c998fabf61ad200118
David Conley <dconley1989@gmail.com>
2021-12-03 10:57:18 -0700

    finished task 5

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,7 +19,35 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-}
+
+  const cardElement = document.createElement("div");
+  const headlineOfCard = document.createElement("div");
+  const authorOfCard = document.createElement("div");
+  const containerOfImage = document.createElement("div");
+  const theImage = document.createElement("img");
+  const spanElement = document.createElement("span");
+
+  cardElement.appendChild(headlineOfCard);
+  cardElement.appendChild(authorOfCard);
+  authorOfCard.appendChild(containerOfImage);
+  containerOfImage.appendChild(theImage);
+  authorOfCard.appendChild(spanElement);
+
+  cardElement.classList.add("card", "open");
+  headlineOfCard.classList.add("headline");
+  authorOfCard.classList.add("author");
+  containerOfImage.classList.add("img-container");
+
+  headlineOfCard.textContent = article.headline;
+  theImage.src = article.authorPhoto;
+  spanElement.textContent = `By ${article.authorName}`;
+
+  cardElement.addEventListener("click", () => {
+    spanElement.classList.toggle("open");
+  });
+
+  return cardElement;
+};
 
 const cardAppender = (selector) => {
   // TASK 6
@@ -28,6 +58,15 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
-}
+  axios
+    .get("http://localhost:5000/api/articles")
+    .then((resp) => {
+      console.log(resp);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally("Woo");
+};
 
-export { Card, cardAppender }
+export { Card, cardAppender };
